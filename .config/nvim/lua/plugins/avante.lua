17770b4eec69a5d4f4e7454ed2c0976a26a69833
p1n9d3v <p1n9@p1n9.local>
2025-04-03 17:46:39 +0900

    feat(avante): claude-3-7 모델 지원 및 설정 추가

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,76 +1,78 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = false,
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "claude",
-    auto_suggestions_provider = "claude",
-    copilot = { model = "claude-3-7-sonnet" },
-    hints = { enabled = false },
-    file_selector = {
-      provider = "snacks",
-      provider_opts = {},
-    },
-    behaviour = {
-      auto_suggestions = false,
-    },
-    suggestion = {
-      debounce = 1000,
-      throttle = 1000,
-    },
-    web_search_engine = {
-      provider = "google", -- tavily, serpapi, searchapi, google or kagi
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+        -- add any opts here
+        -- for example
+        provider = "claude",
+        claude = {
+            endpoint = "https://api.anthropic.com",
+            model = "claude-3-7-sonnet-latest", -- your desired model (or use gpt-4o, etc.)
+            timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+            temperature = 0,
+            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
         },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-    {
-      "folke/which-key.nvim",
-      opts = {
-        spec = {
-          { "<leader>a", group = "ai" },
+        -- hints = { enabled = false },
+        file_selector = {
+            provider = "snacks",
+            provider_opts = {},
         },
-      },
+        behaviour = {
+            auto_suggestions = false,
+        },
+        web_search_engine = {
+            provider = "google", -- tavily, serpapi, searchapi, google or kagi
+        },
     },
-  },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
+        {
+            "folke/which-key.nvim",
+            opts = {
+                spec = {
+                    { "<leader>a", group = "ai" },
+                },
+            },
+        },
+    },
 }
