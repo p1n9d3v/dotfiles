f08a04a6fab5f712f8ad942e3de09f5533c4dd1f
p1n9d3v <p1n9@p1n9.local>
2025-04-14 11:48:01 +0900

    feat: octo 플러그인 설정 추가

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,14 @@
+return {
+    "pwntester/octo.nvim",
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "folke/snacks.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("octo").setup({ enable_builtin = true })
+    end,
+    keys = {
+        { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" },
+    },
+}
